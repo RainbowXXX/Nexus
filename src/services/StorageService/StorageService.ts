ec2dfd6e5ea6ecff1769e9f34c9b114348cc9956
RainbowX <572579046@qq.com>
2024-11-29 12:46:55 +0800

    改变存储逻辑[dev]

---
+++ src/services/StorageService/StorageService.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import * as fs from "node:fs";
+import { Tools } from "../type";
+import { LocalPersistence, UserPersistenceData } from "./StorageModels/LocalPersistence";
+import Result = Tools.Result;
+
+class StorageService {
+	static MATA_DATA_PATH: string = "mata_data/mata_data.json";
+
+	readonly #BasePath: string;
+	readonly #Key: string| undefined;
+
+	mataData: UserPersistenceData;
+
+	constructor(basePath: string, key?: string) {
+		this.#Key = key;
+		this.#BasePath = basePath;
+
+		this.mataData = this.getMataData();
+	}
+
+	getMataData(): UserPersistenceData {
+		let mata_data_path = path.join(this.#BasePath, StorageService.MATA_DATA_PATH);
+		return new LocalPersistence(mata_data_path, this.#Key).getProxy();
+	}
+
+	getDataOfUser(userName: string): any {
+
+	}
+}
